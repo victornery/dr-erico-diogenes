7177064743c70e579ce9cdb1faef7fccce4b358b
Matheus Maia <matheus.maiafelinto@gmail.com>
2018-09-16 15:19:55 -0300

    Adição da seção Cirurgia robotica

--- src/templates/tpl/home/section-robotica.php
+++ src/templates/tpl/home/section-robotica.php
@@ -1,5 +1,9 @@
 <section class="robotica">
     <div class="container">
-        
+        <div class="robotica-conteudo">
+            <h2>Cirurgia Robótica</h2>
+            <p class="text1">A robótica é a evidência do futuro da medicina. A cirurgia aberta não possui tecnologia envolvida e a laparoscopia possui algumas limitações, como a impossibilidade de aproximar a imagem (zoom) e a rigidez da pinça. Na robótica, o médico tem, por meio do console, a possibilidade de simular o movimento do punho, ou seja(retirar ou seja), com visão de 360 graus.</p><br>
+<p>"A era digital chegou à medicina. O robô é uma evolução e uma revolução no treinamento do cirurgião. É o maior respeito que o paciente pode receber", Entre as vantagens da robótica destacam-se a atuação mais precisa e segura do cirurgião, com melhores resultados devido às imagens Full HD em 3D, fundamentais para dar a dimensão de profundidade; de segurança diante do comando do console...</p>
     </div>
+</div>
 </section>
