4b5940e5260e938fcc0da4a6f22caa7a6ddc6543
Matheus Maia <matheus.maiafelinto@gmail.com>
2018-09-23 03:22:02 -0300

    Adicionado seção Especialidades, e editado a parte noticias

--- src/templates/front-page.php
+++ src/templates/front-page.php
@@ -1,4 +1,7 @@
 <?php get_template_part('tpl/global/section','header'); ?>
 <?php get_template_part('tpl/home/section','sobre'); ?>
 <?php get_template_part('tpl/home/section','robotica'); ?>
+<?php get_template_part('tpl/home/section','especialidades'); ?>
+<?php get_template_part('tpl/home/section','noticias'); ?>
+<?php get_template_part('tpl/home/section','whatsapp'); ?>
 <?php get_template_part('tpl/global/section','footer'); ?>
