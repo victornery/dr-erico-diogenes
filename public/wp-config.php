a921810bbc2b3996795ffaf864a678b66e150add
victornery <victornery.t@gmail.com>
2018-09-28 23:55:18 -0300

    Retirando wp config do tracker do git

--- public/wp-config.php
+++ public/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'drerico');
 define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'Denise280600');
+define('DB_PASSWORD', '');
 
 /** Nome do host do MySQL */
 define('DB_HOST', 'localhost');
