090a400dae3394a80dacea0ce3e032f9f3e34d47
Matheus Maia <matheus.maiafelinto@gmail.com>
2018-10-10 21:17:07 -0300

    alteração no wp-config, nav, whatsapp. adicionado 2 post types no arquivo post

--- src/templates/_lib/posts.php
+++ src/templates/_lib/posts.php
@@ -42,10 +42,10 @@ function post_type_banners() {
         'name' => 'Banners',
         'singular_name' => 'Banners',
         'menu_name' => 'Banners',
-        'add_new' => _x('Adicionar Banner', 'item'),
-        'add_new_item' => __('Adicionar Novo Banner'),
-        'edit_item' => __('Editar Banner'),
-        'new_item' => __('Novo Banner')
+        'add_new' => _x('Adicionar convênios', 'item'),
+        'add_new_item' => __('Adicionar Novo convênios'),
+        'edit_item' => __('Editar convênios'),
+        'new_item' => __('Novo convênios')
     );
 
     $args = array(
@@ -67,3 +67,62 @@ function post_type_banners() {
 }
 add_action('init', 'post_type_banners');
 
+function post_type_patologias() {
+    $labels = array(
+        'name' => 'Patologias',
+        'singular_name' => 'patologias',
+        'menu_name' => 'patologias',
+        'add_new' => _x('Adicionar patologia', 'item'),
+        'add_new_item' => __('Adicionar Novo patologia'),
+        'edit_item' => __('Editar patologia'),
+        'new_item' => __('Novo patologia')
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'patologias'),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => true,
+        'menu_position' => 6,
+        'menu_icon' => 'dashicons-slides',
+        'supports' => array('title', 'thumbnail', 'editor')
+    );
+    register_post_type('patologia', $args);
+}
+add_action('init', 'post_type_patologias');
+
+function post_type_convênios() {
+    $labels = array(
+        'name' => 'convênios',
+        'singular_name' => 'convênios',
+        'menu_name' => 'convênios',
+        'add_new' => _x('Adicionar convênios', 'item'),
+        'add_new_item' => __('Adicionar Novo convênios'),
+        'edit_item' => __('Editar convênios'),
+        'new_item' => __('Novo convênios')
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'convênios'),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => true,
+        'menu_position' => 6,
+        'menu_icon' => 'dashicons-slides',
+        'supports' => array('title', 'thumbnail', 'editor')
+    );
+    register_post_type('banner', $args);
+}
+add_action('init', 'post_type_convênios');
