d5788947977a0a48f1cc4e3b69aed0d4b047f9e0
victornery <victornery.t@gmail.com>
2018-10-23 16:24:58 -0300

    Adicionando o mesmo padrão de blgo da home na página do blog

--- src/templates/page-blog.php
+++ src/templates/page-blog.php
@@ -6,12 +6,14 @@
     <ul class="especialidades__lista">
       <?php $theposts = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1)); ?>
       <?php if ($theposts->have_posts()) : while ($theposts->have_posts()): $theposts->the_post() ?>
-      <li>
-        <a href="<?php the_permalink() ?>">
-          <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-          <span><?php the_title(); ?></span>
-        </a>
-      </li>
+        <li>
+          <?php $trim = wp_trim_words(get_the_content(), 40); ?>
+          <a href="<?php the_permalink(); ?>">
+            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" />
+            <h1 class="noticias-titulo"><?php the_title() ?></h1>
+            <p class="text-box"><?php echo $trim; ?> <strong class="btn-rm" rel="noopener" style="margin-left: 0; color: #fff; margin-top: 10px;">Ler mais</strong></p>
+          </a>
+        </li>
       <?php endwhile; endif; wp_reset_postdata(); ?>
     </ul>
   </div>
